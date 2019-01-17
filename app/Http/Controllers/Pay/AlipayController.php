5282a507f7012d550b0d28c5efb78ca8b22e7183
qianqianya <1683472461@qq.com>
2019-01-17 09:02:00 +0800

    sss

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -185,13 +185,13 @@ class AlipayController extends Controller
         //验签
         $res = $this->verify($_POST);
         $log_str = '>>>> ' . date('Y-m-d H:i:s');
-        if ($res === false) {
+        if($res === false){
             //记录日志 验签失败
             $log_str .= " Sign Failed!<<<<< \n\n";
-            file_put_contents('logs/alipay.log', $log_str, FILE_APPEND);
-        } else {
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
+        }else{
             $log_str .= " Sign OK!<<<<< \n\n";
-            file_put_contents('logs/alipay.log', $log_str, FILE_APPEND);
+            file_put_contents('logs/alipay.log',$log_str,FILE_APPEND);
         }
 
         //验证订单交易状态
