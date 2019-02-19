4a480ab179f3d35d124caef9431fa2d9f0900ab1
qianqianya <1683472461@qq.com>
2019-02-19 11:44:57 +0800

    一级菜单

--- app/Http/Controllers/weChat/weChatController.php
+++ app/Http/Controllers/weChat/weChatController.php
@@ -161,7 +161,7 @@ class weChatController extends Controller
             ]
 
         ];
-        var_dump($data);exit;
+        //var_dump($data);exit;
         $r = $client->Request('POST', $url, [
             'body' => json_encode($data,JSON_UNESCAPED_UNICODE)
         ]);
