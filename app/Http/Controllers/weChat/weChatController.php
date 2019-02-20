383a35d84363d85bbdbc3d1077e3f189272769f2
qianqianya <1683472461@qq.com>
2019-02-20 18:34:13 +0800

    aa

--- app/Http/Controllers/weChat/weChatController.php
+++ app/Http/Controllers/weChat/weChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\weChat;
 
 use App\Model\WeixinUser;
+use App\Model\WeixinMedia;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
