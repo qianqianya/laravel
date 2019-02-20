d7b25e00f76788c375a3fb4266f220c617d7a84c
qianqianya <1683472461@qq.com>
2019-02-20 19:41:46 +0800

    aa

--- app/Admin/Controllers/WeimediaController.php
+++ app/Admin/Controllers/WeimediaController.php
@@ -91,7 +91,7 @@ class WeimediaController extends Controller
         $grid->format('Format');
         $grid->msg_id('Msg id');
         $grid->local_file_name('Local file name')->display(function($img){
-            return '<img src="/wx/images/'.$img.'" width=50px;height=50px;>';
+            return '<img src="/wx/images/'.$img.'" width=100px;height=100px;>';
         });
         $grid->local_file_path('Local file path');
 
