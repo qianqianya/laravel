d4575b98b9f2c80045354e3157e934ce1f7b3339
qianqianya <1683472461@qq.com>
2019-02-20 13:32:20 +0800

    aaa

--- app/Http/Controllers/weChat/weChatController.php
+++ app/Http/Controllers/weChat/weChatController.php
@@ -266,6 +266,7 @@ class weChatController extends Controller
     {
         $access_token = $this->getWXAccessToken();
         $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token=' . $access_token;
+        var_dump($url);exit;
         $client = new GuzzleHttp\Client(['base_url' => $url]);
         $param = [
                     "filter"=>[
