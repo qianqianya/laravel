0c791430bd104ce2b01b704ed6ff541944b836bc
qianqianya <1683472461@qq.com>
2019-02-21 11:55:59 +0800

    sss

--- resources/views/mass/mass.blade.php
+++ resources/views/mass/mass.blade.php
@@ -2,6 +2,13 @@
 @extends('layout.goods')
 
 @section('content')
-
-    
+    <form class="form-inline">
+        <div class="form-group">
+            <label class="sr-only" for="goods_num">群发</label>
+            <div class="input-group">
+                群发内容<input type="text" class="form-control" placeholder="请输入您要发布的内容" >
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary" id="add_cart_btn">开始发布</button>
+    </form>
 @endsection
