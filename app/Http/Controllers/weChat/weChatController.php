6f44d73c049f720482061bf9696f9775e81e3de4
qianqianya <1683472461@qq.com>
2019-02-21 13:31:39 +0800

    sss

--- app/Http/Controllers/weChat/weChatController.php
+++ app/Http/Controllers/weChat/weChatController.php
@@ -346,6 +346,7 @@ class weChatController extends Controller
      */
     public function all()
     {
+        $a=$_POST('aaa');
         $access_token = $this->getWXAccessToken();
         $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token='.$access_token;
         //var_dump($url);exit;
@@ -355,7 +356,7 @@ class weChatController extends Controller
                 "is_to_all"=>true
             ],
             "text"=>[
-                "content"=>"aaaa."
+                "content"=>$a
             ],
             "msgtype"=>"text"
         ];
--- resources/views/mass/mass.blade.php
+++ resources/views/mass/mass.blade.php
@@ -2,11 +2,11 @@
 @extends('layout.goods')
 
 @section('content')
-    <form class="form-inline">
+    <form class="form-inline" method="post">
         <div class="form-group">
             <label class="sr-only" for="goods_num">群发</label>
             <div class="input-group">
-                群发内容<input type="text" class="form-control" placeholder="请输入您要发布的内容" >
+                群发内容<input type="text" class="form-control" name="aaa" placeholder="请输入您要发布的内容" >
             </div>
         </div>
 
