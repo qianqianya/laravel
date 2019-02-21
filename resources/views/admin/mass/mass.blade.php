3efd79f0bb6ecfad1790c069453930a7fda0c7bd
qianqianya <1683472461@qq.com>
2019-02-21 16:58:49 +0800

    永久素材

--- resources/views/admin/mass/mass.blade.php
+++ resources/views/admin/mass/mass.blade.php
@@ -1,6 +1,6 @@
 <form action="/admin/wxsendmsg" method="post">
     {{csrf_field()}}
-    <textarea name="mass" id="" cols="10" rows="10"></textarea>
+    <textarea name="mass" id="" cols="20" rows="3"></textarea>
     <input type="file">
     <input type="submit" vlaue="提交">
 </form>
