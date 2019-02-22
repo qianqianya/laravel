5a8aaef6b9a60de38e52897ba480760e83f86f50
qianqianya <1683472461@qq.com>
2019-02-22 18:04:28 +0800

    aaa

--- app/Admin/Controllers/WeixinController.php
+++ app/Admin/Controllers/WeixinController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Redis;
 
 class WeixinController extends Controller
 {
+    protected $redis_weixin_access_token = 'str:weixin_access_token';     //微信 access_token
     use HasResourceActions;
 
     /**
