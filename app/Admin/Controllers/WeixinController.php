950890f8dec0e6cdaad460f2b06cfa706dabe94e
qianqianya <1683472461@qq.com>
2019-02-22 17:00:18 +0800

    aaa

--- app/Admin/Controllers/WeixinController.php
+++ app/Admin/Controllers/WeixinController.php
@@ -9,6 +9,8 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
+use GuzzleHttp;
+use Illuminate\Http\Request;
 
 class WeixinController extends Controller
 {
@@ -159,8 +161,38 @@ class WeixinController extends Controller
     /**
      *
      */
-    public function sendMsg()
+    public function sendMsg(Request $request)
     {
+        $mass=$request->input('mass');
+        $access_token = $this->getWXAccessToken();
+        $url = 'https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token='.$access_token;
+        //var_dump($url);exit;
+        $client = new GuzzleHttp\Client(['base_url' => $url]);
+        $param = [
+            "filter"=>[
+                "is_to_all"=>true
+            ],
+            "text"=>[
+                "content"=>$mass
+            ],
+            "msgtype"=>"text"
+        ];
+        ///var_dump($param);exit;
+        $r = $client->Request('POST', $url, [
+            'body' => json_encode($param, JSON_UNESCAPED_UNICODE)
+        ]);
+        //var_dump($r);exit;
+        $response_arr = json_decode($r->getBody(), true);
+        //echo '<pre>';
+        //print_r($response_arr);
+        // echo '</pre>';
+        if ($response_arr['errcode'] == 0) {
+            echo "发送成功";
+        } else {
+            echo "发送失败";
+            echo '</br>';
+            echo $response_arr['errmsg'];
+        }
         //获取用户openid
         $list = WeixinUser::all()->pluck('openid')->take(10)->toArray();
 
