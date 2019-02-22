2a79dd26b77279f6bda69b60b40685675b6f8d09
qianqianya <1683472461@qq.com>
2019-02-22 15:10:45 +0800

    aaa

--- app/Admin/Controllers/MaterialController.php
+++ app/Admin/Controllers/MaterialController.php
@@ -83,7 +83,7 @@ class MaterialController extends Controller
 
         $grid->id('Id');
         $grid->url('Url')->display(function($url_img){
-            return '<img src="'.$url_img.'">';
+            return '<img src="/'.$url_img.'">';
         });
         $grid->add_time('Add time');
 
