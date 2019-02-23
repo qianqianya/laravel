9f56e434b3f3b24286cbf49732f780b4f442a343
qianqianya <1683472461@qq.com>
2019-02-23 10:38:34 +0800

    aaa

--- resources/views/admin/mass/oneShot.blade.php
+++ resources/views/admin/mass/oneShot.blade.php
@@ -125,14 +125,14 @@
 
         if (that.hasClass("picture")) {
             console.log("图片长按");
-            menuTpl = " <div class='menu menu-picture'><a href='#'><i class='ico i1'></i>复制</a><a href='#'><i class='ico i2'></i>收藏</a><ahref='#'><i class='ico i3'></i>另存为</a>" + _revoke + "<a href='#'><i class='ico i5'></i>删除</a></div>";
+            menuTpl = " <div class='menu menu-picture'><a href='#'><i class='ico i1'></i>复制</a><a href='#'><i class='ico i2'></i>收藏</a><a href='#'><i class='ico i3'></i>另存为</a>" + _revoke + "<a href='#'><i class='ico i5'></i>删除</a></div>";
         }else if(that.hasClass("video"))
         {
             console.log("视频长按");
             menuTpl = " <div class='menu menu-video'><a href='#'><i class='ico i3'></i>另存为</a>" + _revoke + "<a href='#'><i class='ico i5'></i>删除</a> </div>";
         }else{
             console.log("文字长按");
-            menuTpl = " < div class = 'menu menu-text' > < a href = '#' > < i class = 'ico i1' > < / i > 复制 < / a > <a href = '#' > < i class = 'ico i2' > < / i > 收藏 < / a > " + _revoke
+            menuTpl = " <div class = 'menu menu-text' > <a href = '#' > <i class = 'ico i1' ></i > 复制 </a > <a href = '#' > <i class = 'ico i2' > </i > 收藏 </a > " + _revoke
                     + "<a href='#'><i class='ico i5'></i>删除</a></div>";
         }
 
@@ -144,7 +144,7 @@
             autoPos();
         }
 
-        functionautoPos()
+        function autoPos()
         {
             console.log(that.position().top)
             var_other = that.parents("li").hasClass("others");
