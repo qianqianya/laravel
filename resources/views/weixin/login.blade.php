604a16e82e3b9fb4e0df0d91991a8a2e4b8dbab9
qianqianya <1683472461@qq.com>
2019-02-28 14:11:00 +0800

    微信登陆

--- resources/views/weixin/login.blade.php
+++ resources/views/weixin/login.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h2>微信登录</h2>
         <h3>
-            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3A%2F%2Fmall.77sc.com.cn%2Fweixin.php%3Fr1%3Dqaz.qianqianya.xyz%2FwxGetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
+            <a href="https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&amp;redirect_uri=http%3A%2F%2Fmall.77sc.com.cn%2Fweixin.php%3Fr1%3Dhttp%3A%2F%2Fqaz.qianqianya.xyz%2FwxGetcode&amp;response_type=code&amp;scope=snsapi_login&amp;state=STATE#wechat_redirect">Login</a>
         </h3>
     </div>
 @endsection
