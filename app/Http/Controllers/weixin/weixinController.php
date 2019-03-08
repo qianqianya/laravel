2c21a28ffee100f52d4ac86a43c3933653117792
qianqianya <1683472461@qq.com>
2019-03-08 11:29:26 +0800

    aaa

--- app/Http/Controllers/weixin/weixinController.php
+++ app/Http/Controllers/weixin/weixinController.php
@@ -12,8 +12,67 @@ use Illuminate\Support\Facades\Storage;
 class weixinController extends Controller
 {
     //protected $redis_weixin_access_token='str:weixin_access_token';
+    public function weChat(){
+        echo $_GET['echostr'];
+    }
+
+    public function validToken()
+    {
+        //$get = json_encode($_GET);
+        //$str = '>>>>>' . date('Y-m-d H:i:s') .' '. $get . "<<<<<\n";
+        //file_put_contents('logs/weixin.log',$str,FILE_APPEND);
+        //echo $_GET['echostr'];
+        $data = file_get_contents("php://input");
+        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
+        file_put_contents('logs/wx_event.log', $log_str, FILE_APPEND);
+    }
+
+    public function weChatToken()
+    {
+        $data = file_get_contents("php://input");
+
+        $xml = simplexml_load_string($data);
+        $event = $xml->Event;
+        //var_dump($xml);echo '<hr>';
+
+        $openid = $xml->FromUserName;
+        $sub_time = $xml->CreateTime;
+
+        echo 'openid: ' . $openid;
+        echo '</br>';
+        echo 'sub_time: ' . $sub_time;
+
+        //获取用户信息
+        $user_info = $this->getUserInfo($openid);
+        echo '<pre>';
+        print_r($user_info);
+        echo '</pre>';
+        //保存用户信息
+        $u = WeixinUser::where(['openid' => $openid])->first();
+        //var_dump($u);die;
+        if ($u) {
+            echo '用户已存在';
+        } else {
+            $user_data = [
+                'openid' => $openid,
+                'add_time' => time(),
+                'nickname' => $user_info['nickname'],
+                'sex' => $user_info['sex'],
+                'headimgurl' => $user_info['headimgurl'],
+                'subscribe_time' => $sub_time,
+            ];
+
+            $id = WeixinUser::insertGetId($user_data);
+            var_dump($id);
+        }
+
+
+    }
+
     public function getWXAccessToken()
     {
+
+
         $token = Redis::get($this->redis_weixin_access_token);
         if (!$token) {
             $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . env('WEIXIN_APPID') . '&secret=' . env('WEIXIN_APPSECRET');
@@ -34,9 +93,10 @@ class weixinController extends Controller
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' . $access_token . '&openid=' . $openid . '&lang=zh_CN';
 
         $data = json_decode(file_get_contents($url), true);
-        $res = WeixinUser::where(['openid' => $openid])->select();
-
-     var_dump($res);
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+        return $data;
     }
 
 }
