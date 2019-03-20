f28fd394ec42e61dedd5afd415de08d2e46cd994
qianqianya <1683472461@qq.com>
2019-03-20 15:52:06 +0800

    zhenji

--- app/Http/Controllers/zhenji/ZhenjiController.php
+++ app/Http/Controllers/zhenji/ZhenjiController.php
@@ -7,9 +7,6 @@ use App\Http\Controllers\Controller;
 class ZhenjiController extends Controller
 {
     public function zhenji(){
-        $data=[
-            'title'=>'登录页面'
-        ];
-        return view('login.login',$data);
+        return json_encode(111);
     }
 }
